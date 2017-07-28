e06c70dbfd80d84b17ebb1d6b13a32d860341f7d
fripig <fripig@gmail.com>
2017-07-28 17:55:46 +0800

    test upload

--- javascripts/util.js
+++ javascripts/util.js
@@ -95,7 +95,7 @@
       return $.ajax({
         url: endpoint,
         type: 'POST',
-        data: {image:base64.replace('data%3Aimage%2Fpng%3B',''),type:'base64'},
+        data: {image:base64.replace('data:image/png;',''),type:'base64'},
         headers: {
           'Content-Type': 'multipart/form-data',
             'Authorization':'Client-ID d8b6ccf7579e48e'
